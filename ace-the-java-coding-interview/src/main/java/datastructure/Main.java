5d65fa675c952f36277538b9d323329f60383f95
msk <safdar.khan@karexpert.com>
2020-02-19 08:19:53 +0530

    First commit

--- ace-the-java-coding-interview/src/main/java/datastructure/Main.java
+++ ace-the-java-coding-interview/src/main/java/datastructure/Main.java
@@ -1,5 +1,27 @@
 package datastructure;
 
 public class Main {
-
+	public static void main(String[] args) {
+		number_1_remove_even_integers_from_array();
+	}
+	
+	public static void number_1_remove_even_integers_from_array() {
+		int[] arr = {1, 2, 4, 5, 10, 6, 3};
+		int n=0;
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]%2!=0)
+				n++;
+		}
+		int[] oddArr = new int[n];
+		int index = 0;
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]%2!=0) {
+				oddArr[index]=arr[i];
+				index++;
+			}
+		}
+		for(int a:oddArr) {
+			System.out.println(a);
+		}
+	}
 }
