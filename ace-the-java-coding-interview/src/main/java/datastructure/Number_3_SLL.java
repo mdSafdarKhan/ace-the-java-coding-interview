17bc99fdda58b527ad7143946ebdc74cf1e52f1c
mdSafdarKhan <safdar.khan@karexpert.com>
2020-02-21 22:07:45 +0530

    sll

--- ace-the-java-coding-interview/src/main/java/datastructure/Number_3_SLL.java
+++ ace-the-java-coding-interview/src/main/java/datastructure/Number_3_SLL.java
@@ -7,7 +7,14 @@ public class Number_3_SLL {
 		singlyLinkedList.insertAtHead("a");
 		singlyLinkedList.insertAtHead("b");
 		singlyLinkedList.insertAtHead("c");
+		singlyLinkedList.insertAtEnd("y");
+		singlyLinkedList.insertAtEnd("z");
+		singlyLinkedList.insertAtHead("1");
+		singlyLinkedList.insertAfter("omega", "y");
+		singlyLinkedList.insertAfter("theta", "z");
 		singlyLinkedList.printList();
+		singlyLinkedList.searchNode("omega");
+		singlyLinkedList.searchNode("gama");
 	}
 }
 
@@ -26,12 +33,55 @@ class SinglyLinkedList{
 		headNode = node;
 	}
 	
+	public void insertAtEnd(String data) {
+		
+		Node node = new Node();
+		node.data = data;
+		
+		Node currentNode = headNode;
+		while(currentNode != null) {
+			if(currentNode.nextNode == null) {
+				break;
+			}
+			currentNode = currentNode.nextNode;
+		}
+		currentNode.nextNode = node;
+	}
+	
+	public void insertAfter(String data, String previous) {
+		Node temp = headNode;
+		while(temp != null) {
+			String nodeData = temp.data;
+			if(nodeData.equals(previous)) {
+				Node newNode = new Node();
+				newNode.data = data;
+				newNode.nextNode = temp.nextNode;
+				
+				temp.nextNode = newNode;
+			}
+			temp = temp.nextNode;
+		}
+	}
+	
+	public void searchNode(String data) {
+		boolean found = false;
+		Node temp = headNode;
+		while(temp != null) {
+			if(temp.data.equals(data)) {
+				found = true;
+			}
+			temp = temp.nextNode;
+		}
+		System.out.println(data + " found: " + found);
+	}
+	
 	public void printList() {
 		Node temp = headNode;
 		while(temp != null) {
-			System.out.println(temp.data);
+			System.out.print(temp.data + " -> ");
 			temp = temp.nextNode;
 		}
+		System.out.println(temp);
 	}
 }
 
